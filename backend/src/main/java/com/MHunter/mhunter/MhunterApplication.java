28be3e4e12ae7d3b46538ae07c96379ab96d0f72
kasun789 <kasungayashan789@gmail.com>
2023-07-05 22:33:17 +0530

    build backend folder structure

---
+++ backend/src/main/java/com/MHunter/mhunter/MhunterApplication.java
@@ -0,0 +1,13 @@
+package com.MHunter.mhunter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MhunterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MhunterApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/MHunter/mhunter/MhunterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MHunter.mhunter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MhunterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
