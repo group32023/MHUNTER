8a9f42d63de55e36da92b3887dbd5f73154d8a13
Nirodha <2020cs064@stu.ucsc.cmb.ac.lk>
2023-07-15 13:51:02 +0530

    create event

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,33 @@
 import './App.css';
 import NavBar from './components/common/NavBar';
 import MainSlider from './components/common/MainSlider'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CreateEvent from './pages/Organizer/CreateEvent';
+
 function App() {
   return (
-    <div className="App">
-      <div>
-      <MainSlider></MainSlider>
-      <div>
-        <NavBar></NavBar>
+    
+    <BrowserRouter>
+
+      <div className="App">
+        <div>
+          <MainSlider></MainSlider>
+          <div>
+            <NavBar></NavBar>
+          </div>
+        </div>
+
       </div>
-      </div>
-     
-     
-    </div>
+      <Routes>
+
+        <Route path="/Organizer/CreateEvent" element={<CreateEvent/>} >
+        </Route>
+      </Routes>
+
+    </BrowserRouter>
+
   );
+
 }
 
 export default App;
