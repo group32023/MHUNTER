5d182b3c10b84961b056d324722ee78c16a9c02d
Nirodha <2020cs064@stu.ucsc.cmb.ac.lk>
2023-07-15 20:12:50 +0530

    create event

--- frontend/src/pages/Organizer/CreateEvent.js
+++ frontend/src/pages/Organizer/CreateEvent.js
@@ -1,12 +1,29 @@
+import './CreateEvent.css';
 
+function CreateEvent() {
 
-function CreateEvent(){
+  return (
+    <div className="container">
 
-  return(
-    <div>
+      <form>
 
-          <h1>Text</h1>   
-    
+        <h2>Create Event</h2>
+        <div className="form-group">
+
+          <label htmlFor="eventname" className="form-label">Event Name</label>
+          <input type="text" className="form-control" id="eventname" placeholder="Enter event name"></input>
+
+        </div>   
+
+        <div className="form-group">
+
+          <label htmlFor="eventcrowd" className="form-label">Expected Crowd</label>
+          <input type="text" className="form-control" id="eventcrowd" placeholder="Enter expected crowd"></input>
+
+        </div>
+
+      </form>
+
     </div>
 
   );
