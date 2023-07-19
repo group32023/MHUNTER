e9369c5c907ab5c349d8ba4e8f906f81dd776d86
Nirodha <2020cs064@stu.ucsc.cmb.ac.lk>
2023-07-20 01:11:48 +0530

    commits

--- frontend/src/components/Organizer/LocationInput.js
+++ frontend/src/components/Organizer/LocationInput.js
@@ -1,6 +1,8 @@
 import React, { useState, useRef } from 'react';
 import { GoogleMap, Marker, InfoWindow, LoadScript, StandaloneSearchBox } from '@react-google-maps/api';
 
+const libraries = ['places'];
+
 const LocationInput = ({ onLocationSelect }) => {
   const [selectedLocation, setSelectedLocation] = useState(null);
   const mapRef = useRef(null);
@@ -15,22 +17,29 @@ const LocationInput = ({ onLocationSelect }) => {
       const places = searchBoxRef.current.getPlaces();
       if (places.length > 0) {
         const place = places[0];
-        setSelectedLocation({
+        const newLocation = {
           lat: place.geometry.location.lat(),
           lng: place.geometry.location.lng(),
           address: place.formatted_address,
-        });
-        onLocationSelect({
-          lat: place.geometry.location.lat(),
-          lng: place.geometry.location.lng(),
-          address: place.formatted_address,
-        });
+        };
+        setSelectedLocation(newLocation);
+        onLocationSelect(newLocation);
       }
     }
   };
 
+  const handleMapClick = (event) => {
+    const newLocation = {
+      lat: event.latLng.lat(),
+      lng: event.latLng.lng(),
+      address: '', // You can set the address as an empty string for now or use a reverse geocoding API to get the address based on the latLng.
+    };
+    setSelectedLocation(newLocation);
+    onLocationSelect(newLocation);
+  };
+
   return (
-    <LoadScript googleMapsApiKey="AIzaSyBHUE7QQ_cie9vWpTKHX7rOuqAoD8uxhCA" libraries={['places']}>
+    <LoadScript googleMapsApiKey="AIzaSyBHUE7QQ_cie9vWpTKHX7rOuqAoD8uxhCA" libraries={libraries}>
       <div>
         <StandaloneSearchBox onLoad={(searchBox) => (searchBoxRef.current = searchBox)} onPlacesChanged={handlePlaceSelect}>
           <input type="text" placeholder="Enter a location" />
@@ -38,6 +47,7 @@ const LocationInput = ({ onLocationSelect }) => {
         {selectedLocation && (
           <GoogleMap
             onLoad={handleMapLoad}
+            onClick={handleMapClick} // Add the click event handler to the map
             center={selectedLocation}
             zoom={15}
             mapContainerStyle={{ height: '300px', width: '100%' }}
