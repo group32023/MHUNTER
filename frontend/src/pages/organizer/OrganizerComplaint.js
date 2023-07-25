7f93f920fdceaa56274ccf2d7d34372c29dcd2fc
Umasha Kaumadi <105776548+umasha2000@users.noreply.github.com>
2023-07-26 03:46:29 +0530

    Committing Changes

---
+++ frontend/src/pages/organizer/OrganizerComplaint.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const OrganizerComplaint = () => {
+    return (
+        <div className='content_container text-white'>OrganizerComplaint</div>
+    )
+}
+
+export default OrganizerComplaint
