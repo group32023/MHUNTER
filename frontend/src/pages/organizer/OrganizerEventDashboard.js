c0feaf771f9eb928337acefe9b9f93cbc7a3e1ec
Umasha Kaumadi <105776548+umasha2000@users.noreply.github.com>
2023-08-01 12:33:25 +0530

    Event Dashboard Changes

--- frontend/src/pages/organizer/OrganizerEventDashboard.js
+++ frontend/src/pages/organizer/OrganizerEventDashboard.js
@@ -1,14 +1,26 @@
 import React from 'react'
 import "../../assets/css/OrganizerEventDashboard.css"
-
+import Topbar from '../../components/common/Topbar';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function OrganizerEventDashboard() {
     return (
-        <>
-            <div className='content_container'>
-                <h1>Home</h1>
+        <div className='eventDashboard_content_container'>
+            <Topbar />
+
+            <div className="row">
+                <div className="horizontalLine col-md-12 py-2 fs-7">
+                    <span >Events / BEAT BLITZ</span>
+                </div>
             </div>
-        </>
+
+            <div className="row mt-4">
+                <div className="horizontalLine col-md-12 py-2 fs-7">
+                    <span >Events / BEAT BLITZ</span>
+                </div>
+            </div>
+        </div>
+
 
     )
 }
