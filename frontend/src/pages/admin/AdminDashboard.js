8e20f8224abafd998420b36a5bb2fb009f9c234c
Diani Dickovita <108466170+DianiDickovita@users.noreply.github.com>
2023-08-02 16:20:16 +0530

    admin_calendar

--- frontend/src/pages/admin/AdminDashboard.js
+++ frontend/src/pages/admin/AdminDashboard.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SideMenuBarAdmin from '../../components/common/SideMenuBar/SideMenuBarAdmin';
 import '../../assets/css/admin/adminDashboard.css'
 import notification from '../../assets/images/notification-a.png'
 import home from '../../assets/images/home-button-a.png'
 import logout from '../../assets/images/logout-a.png'
+import Calendar from 'react-calendar';
 
 export default function AdminDashboard() {
+  const[date, setDate] = useState(new Date());
+  const onChange = date => {
+    setDate(date);
+  }
   return (
     <div className='main-container'>
       <div className='side-bar'>
@@ -27,18 +32,23 @@ export default function AdminDashboard() {
 
         </div>
 
-        <div className='body-admin'>
-          <div className="container text-center">
-            <div className="row">
-                      <div className="col-sm-4 bg-black">
-                        <p>Current Project</p>
-                      </div>
-                      <div className="col-sm-4 top-artists">          
-                        <p>Project 2</p>    
-                      </div>
-                      <div className="col-sm-4">
-                        <p>Calender</p>
-                      </div>
+
+        <div className="container ">
+          <div className="row">
+            <div className="col-sm-4 ">
+              <div className='top-band-list'>
+                <h4 className='text-center'>Top 10 Bands</h4>
+              </div>
+            </div>
+            <div className="col-sm-4">
+              <div className='top-band-list'>
+                <h4 className='text-center'>Top 10 Artists</h4>
+              </div>
+            </div>
+            <div className="col-sm-4">
+              <div className='admin-calendar text-center'>
+                <Calendar onChange={onChange} value={date} />
+              </div>
             </div>
           </div>
         </div>
