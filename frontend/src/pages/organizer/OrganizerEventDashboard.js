ccd47e2a1abb50fcdfe39a7be8fddfd35a723ab6
Umasha Kaumadi <105776548+umasha2000@users.noreply.github.com>
2023-08-06 04:23:37 +0530

    Event Dashboard page styles

--- frontend/src/pages/organizer/OrganizerEventDashboard.js
+++ frontend/src/pages/organizer/OrganizerEventDashboard.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import "../../assets/css/OrganizerEventDashboard.css"
 import Topbar from '../../components/common/Topbar';
+import profilePhoto from '../../assets/images/profilePhoto.jpeg'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function OrganizerEventDashboard() {
@@ -15,14 +16,37 @@ function OrganizerEventDashboard() {
             </div>
 
             <div className="row">
-                <div className="eventDescriptionDiv  mt-4 col-md-8">
-                    <div className="p-4">
-                        Content for 8-cols div
+                <div className="eventDescriptionDiv  mt-4 col-md-7">
+                    <div className="row p-3">
+                        <div className="eventImgDiv col-md-2 mt-2">
+                            <img alt='' src={profilePhoto} width='130px' height='130px' />
+                        </div>
+                        <div className="eventDescDiv col-md-10">
+                            <span>BEAT BLITZ</span>
+                            {/* Content for 8-cols div
+                            Content for 8-cols div
+                            Content for 8-cols div
+                            Content for 8-cols div
+                            Content for 8-cols div
+                            Content for 8-cols div
+                            Content for 8-cols div
+                            Content for 8-cols div
+                            Content for 8-cols div
+                            Content for 8-cols div
+                            Content for 8-cols div
+                            Content for 8-cols div
+                            Content for 8-cols div
+                            Content for 8-cols div
+                            Content for 8-cols div
+                            Content for 8-cols div
+                            Content for 8-cols div
+                            Content for 8-cols div */}
+                        </div>
                     </div>
                 </div>
 
-                <div className="locationDescriptionDiv mt-4 col-md-3">
-                    <div className="eventTypeDescriptionDiv ">
+                <div className="locationDescriptionDiv mt-4 col-md-4">
+                    <div className="locationTypeDescriptionDiv">
                         <div className="p-4">
                             Content for 4-cols div
                         </div>
@@ -37,19 +61,48 @@ function OrganizerEventDashboard() {
 
 
             <div className="row">
-                <div className="eventDescriptionDiv  mt-4 col-md-8">
+                <div className="requestTypeDescriptionDiv  mt-2 col-md-7" id='scrollbarStyle-1'>
                     <div className="p-4">
-                        Content for 8-cols div
+                        Content for 8-cols div<hr></hr>
+                        Content for 8-cols div<hr></hr>
+                        Content for 8-cols div<hr></hr>
+                        Content for 8-cols div<hr></hr>
+                        Content for 8-cols div<hr></hr>
+                        Content for 8-cols div<hr></hr>
+                        Content for 8-cols div<hr></hr>
+                        Content for 8-cols div<hr></hr>
+                        Content for 8-cols div<hr></hr>
+                        Content for 8-cols div<hr></hr>
+                        Content for 8-cols div<hr></hr>
+                        Content for 8-cols div<hr></hr>
+                        Content for 8-cols div<hr></hr>
+                        Content for 8-cols div<hr></hr>
+                        Content for 8-cols div<hr></hr>
+                        Content for 8-cols div<hr></hr>
+                        Content for 8-cols div<hr></hr>
+                        Content for 8-cols div<hr></hr>
+                        Content for 8-cols div<hr></hr>
+                        Content for 8-cols div<hr></hr>
+                        Content for 8-cols div<hr></hr>
+                        Content for 8-cols div<hr></hr>
+                        Content for 8-cols div<hr></hr>
+                        Content for 8-cols div<hr></hr>
+
                     </div>
                 </div>
 
-                <div className="locationDescriptionDiv mt-4 col-md-3">
-                    <div className="eventTypeDescriptionDiv ">
+                <div className="locationDescriptionDiv mt-2 col-md-4">
+                    <div className="crowdTypeDescriptionDiv ">
                         <div className="p-4">
                             Content for 4-cols div
                         </div>
                     </div>
-                    <div className="eventTypeDescriptionDiv ">
+                    <div className="addTypeDescriptionDiv ">
+                        <div className="p-4">
+                            Content for 4-cols div
+                        </div>
+                    </div>
+                    <div className="btnTypeDescriptionDiv ">
                         <div className="p-4">
                             Content for 4-cols div
                         </div>
