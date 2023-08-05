0f18554bb10d794bd751044d759e0eeacdc4ccbd
Umasha Kaumadi <105776548+umasha2000@users.noreply.github.com>
2023-08-05 22:31:22 +0530

    event dashboard changes

--- frontend/src/pages/organizer/OrganizerEventDashboard.js
+++ frontend/src/pages/organizer/OrganizerEventDashboard.js
@@ -14,10 +14,31 @@ function OrganizerEventDashboard() {
                 </div>
             </div>
 
-            <div className="row mt-4">
-                <div className="horizontalLine col-md-12 py-2 fs-7">
-                    <span >Events / BEAT BLITZ</span>
+            <div class="row">
+
+
+                <div className="col-8 mt-4 mx-4">
+                    <div className="eventDescriptionDiv p-4">
+                        Content for 8-cols div
+                    </div>
                 </div>
+
+
+                {/* <div class="col-xs-4">
+                <div class="col-xs-4">
+                            <div className="locationDescriptionDiv">
+                                Content for 4-cols div
+                            </div>
+                        </div>
+
+
+                        <div class="col-xs-4">
+                            <div className="locationDescriptionDiv">
+                                Content for half-width 4-cols div
+                            </div>
+                        </div>
+                    </div>
+                </div> */}
             </div>
         </div>
 
