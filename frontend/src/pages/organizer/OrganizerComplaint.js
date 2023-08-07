3e12bbbee39cff9619586d0aeaf96dd887051f49
Umasha Kaumadi <105776548+umasha2000@users.noreply.github.com>
2023-08-07 15:27:21 +0530

    Organizer Complaint Committing Changes

--- frontend/src/pages/organizer/OrganizerComplaint.js
+++ frontend/src/pages/organizer/OrganizerComplaint.js
@@ -1,11 +1,23 @@
 import React from 'react'
-import SideMenuBarOrganizer from '../../components/common/SideMenuBar/SideMenuBarOrganizer'
+import "../../assets/css/OrganizerComplaint.css"
+import Topbar from '../../components/common/Topbar';
 
-const OrganizerComplaint = () => {
+function OrganizerComplaint() {
     return (
-        <div>
-            <div className='mainContainer'>
-                <SideMenuBarOrganizer></SideMenuBarOrganizer>
+        <div className='complaint_content_container'>
+            <Topbar />
+
+            <div className='complaint_content_container_main_row row mt-4 p-3'>
+                <div className='complaintForm col-md-4 p-3'>
+                    <div className="p-4 text-center">
+                        <p className='fs-5' style={{ fontFamily: 'MyCustomFont1' }}>Complaint Form</p>
+                        <hr></hr>
+                    </div>
+                </div>
+
+                <div className='complaintTableDiv col-md-7 p-3 mx-4'>
+                    content
+                </div>
 
             </div>
         </div>
