96ff0db6afbafe9bb5aaac28fabf99959dd6f3ca
Umasha Kaumadi <105776548+umasha2000@users.noreply.github.com>
2023-08-10 14:01:12 +0530

    Complaint Module Changes

--- backend/src/main/java/com/MHunter/mhunter/model/Complaint.java
+++ backend/src/main/java/com/MHunter/mhunter/model/Complaint.java
@@ -1,4 +1,42 @@
 package com.MHunter.mhunter.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Complaint {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int complaintID;
+    private String title;
+    private String description;
+
+    public Complaint() {
+    }
+
+    public int getComplaintID() {
+        return complaintID;
+    }
+
+    public void setComplaintID(int complaintID) {
+        this.complaintID = complaintID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
