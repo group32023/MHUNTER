cfa9694b7e03bf1b624daf2903cec1897c13f396
Nirodha <2020cs064@stu.ucsc.cmb.ac.lk>
2023-08-11 23:43:10 +0530

    commits

--- frontend/src/pages/organizer/MakeArtistRequest.js
+++ frontend/src/pages/organizer/MakeArtistRequest.js
@@ -10,66 +10,66 @@ function MakeArtistRequest() {
 
 
     const currentDate = format(new Date(), 'yyyy-MM-dd');
-    const [formData, setFormData] = useState({
+    // const [formData, setFormData] = useState({
 
-        event_name: '',
-        event_type: '',
-        date: '',
-        crowd: '',
-        start_time: '',
-        end_time: '',
-        location: '',
-        description: '',
-        latitude: '',
-        longitude: '',
+    //     event_name: '',
+    //     event_type: '',
+    //     date: '',
+    //     crowd: '',
+    //     start_time: '',
+    //     end_time: '',
+    //     location: '',
+    //     description: '',
+    //     latitude: '',
+    //     longitude: '',
 
-    })
+    // })
 
-    const [crowdError, setCrowdError] = useState('');
+    // const [crowdError, setCrowdError] = useState('');
 
-    const onChangeHandler = (event) => {
-        if (!event.target) return; // Check if event.target exists
+    // const onChangeHandler = (event) => {
+    //     if (!event.target) return; // Check if event.target exists
 
-        const { name, value } = event.target;
+    //     const { name, value } = event.target;
 
-        if (name === 'crowd') {
+    //     if (name === 'crowd') {
 
-            if (value <= 0) {
-                setCrowdError('Expected crowd should be a positive integer');
-            }
+    //         if (value <= 0) {
+    //             setCrowdError('Expected crowd should be a positive integer');
+    //         }
 
-            else if (!Number.isInteger(value)) {
-                setCrowdError('Expected crowd should be a positive integer');
-            }
+    //         else if (!Number.isInteger(value)) {
+    //             setCrowdError('Expected crowd should be a positive integer');
+    //         }
 
-            else {
-                setCrowdError('');
-                // Handle location selection
-                setFormData((prevFormData) => ({
-                    ...prevFormData,
-                    [name]: value,
-                }));
-            }
+    //         else {
+    //             setCrowdError('');
+    //             // Handle location selection
+    //             setFormData((prevFormData) => ({
+    //                 ...prevFormData,
+    //                 [name]: value,
+    //             }));
+    //         }
 
-        } else {
-            // Handle form input changes
-            setFormData((prevFormData) => ({
-                ...prevFormData,
-                [name]: value,
-            }));
-        }
-    };
+    //     } else {
+    //         // Handle form input changes
+    //         setFormData((prevFormData) => ({
+    //             ...prevFormData,
+    //             [name]: value,
+    //         }));
+    //     }
+    // };
 
-    const handleLocationSelect = (selectedLocation) => {
-        // Update the location property in the formData state with the selected location data
-        setFormData((prevFormData) => ({
-            ...prevFormData,
-            location: selectedLocation.address,
-        }));
+    // const handleLocationSelect = (selectedLocation) => {
+    //     // Update the location property in the formData state with the selected location data
+    //     setFormData((prevFormData) => ({
+    //         ...prevFormData,
+    //         location: selectedLocation.address,
+    //     }));
 
-        // Print the location value in the console
-        console.log(selectedLocation.address);
-    };
+    //     // Print the location value in the console
+    //     console.log(selectedLocation.address);
+    // };
 
     return (
 
@@ -85,14 +85,14 @@ function MakeArtistRequest() {
                 <div className="form-group ">
 
                     <label htmlFor="event_name" className="form-label">Event Name</label>
-                    <input type="text" className="form-control" onChange={onChangeHandler} name="event_name" ></input>
+                    <input type="text" className="form-control"  name="event_name" ></input>
 
                 </div>
 
                 <div className="form-group">
 
                     <label htmlFor="event_type" className="form-label">Event Type</label>
-                    <select className="form-select" onChange={onChangeHandler} name="event_type" >
+                    <select className="form-select"  name="event_type" >
 
                         <option value="musical">Musical Show</option>
                         <option value="party">Party</option>
@@ -108,14 +108,14 @@ function MakeArtistRequest() {
 
                     <div className="form-group col-md-6">
                         <label htmlFor="date" className="form-label" >Date</label>
-                        <input type="date" className="form-control" onChange={onChangeHandler} name="date" min={currentDate} ></input>
+                        <input type="date" className="form-control" name="date" min={currentDate} ></input>
                     </div>
 
                     <div className="form-group col-md-6">
 
                         <label htmlFor="crowd" className="form-label">Expected Crowd</label>
-                        <input type="text" className="form-control" onChange={onChangeHandler} name="crowd" ></input>
-                        {crowdError && <div className='invalid-feedback'>{crowdError}</div>}
+                        <input type="text" className="form-control"  name="crowd" ></input>
+                        
                     </div>
                 </div>
 
@@ -124,13 +124,13 @@ function MakeArtistRequest() {
 
                     <div className="form-group col-md-6">
                         <label htmlFor="start_time" className="form-label" >Starting Time</label>
-                        <input type="time" className="form-control" onChange={onChangeHandler} name="start_time" ></input>
+                        <input type="time" className="form-control"  name="start_time" ></input>
                     </div>
 
                     <div className="form-group col-md-6">
 
                         <label htmlFor="end_time" className="form-label">Ending Time</label>
-                        <input type="time" className="form-control" onChange={onChangeHandler} name="end_time" ></input>
+                        <input type="time" className="form-control" name="end_time" ></input>
 
                     </div>
                 </div>
@@ -139,13 +139,13 @@ function MakeArtistRequest() {
 
                     <div className="form-group col-md-6">
                         <label htmlFor="start_time" className="form-label" >Artist Arrival Time</label>
-                        <input type="time" className="form-control" onChange={onChangeHandler} name="start_time" ></input>
+                        <input type="time" className="form-control" name="start_time" ></input>
                     </div>
 
                     <div className="form-group col-md-6">
 
                         <label htmlFor="end_time" className="form-label">Artist Departure Time</label>
-                        <input type="time" className="form-control" onChange={onChangeHandler} name="end_time" ></input>
+                        <input type="time" className="form-control"  name="end_time" ></input>
 
                     </div>
                 </div>
@@ -153,26 +153,20 @@ function MakeArtistRequest() {
                 <div className="form-group">
 
                     <label htmlFor="location" className="form-label">Location</label>
-                    <LocationInput onLocationSelect={handleLocationSelect} setFormData={setFormData} />
+                    {/* <LocationInput  setFormData={setFormData} /> */}
 
                 </div>
 
                 <div className="form-group">
 
                     <label htmlFor="description" className="form-label">Special Notes</label>
-                    <textarea type="text" className="form-control" onChange={onChangeHandler} name="description" ></textarea>
+                    <textarea type="text" className="form-control"  name="description" ></textarea>
 
                 </div>
 
                 <div className="form-group">
 
-                    <button class="btn " type="submit" onClick={(event) => {
-                        event.preventDefault(); console.log(formData); fetch("http://localhost:8080/event/add", {
-                            method: "POST",
-                            headers: { "Content-Type": "application/json" },
-                            body: JSON.stringify(formData)
-                        })
-                    }} >Submit</button>
+                    <button class="btn " type="submit" >Submit</button>
 
                 </div>
 
