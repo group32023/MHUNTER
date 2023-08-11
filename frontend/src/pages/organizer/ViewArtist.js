1fd0d1468f919b2de888f3ea6b382995e529a076
Nirodha <2020cs064@stu.ucsc.cmb.ac.lk>
2023-08-11 11:27:38 +0530

    commits

---
+++ frontend/src/pages/organizer/ViewArtist.js
@@ -0,0 +1,22 @@
+
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import Topbar from '../../components/common/Topbar';
+import './ViewArtist.css';
+import theekshana from '../../assets/images/theekshana.jpg';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+library.add(fas); 
+
+function SearchArtist() {
+
+    return (
+
+        
+
+    );
+
+}
+
+export default ViewArtist;
