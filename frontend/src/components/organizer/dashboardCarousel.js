beaf662cf1aa9648306189a887b26d11d65bc151
Umasha Kaumadi <105776548+umasha2000@users.noreply.github.com>
2023-08-11 22:15:13 +0530

    organizer dashboard carousel component

---
+++ frontend/src/components/organizer/dashboardCarousel.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react';
+import { Carousel, Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import image1 from '../../assets/images/carousel1.jpg';
+import image2 from '../../assets/images/carousel2.jpg';
+import image3 from '../../assets/images/carousel3.jpg';
+import image4 from '../../assets/images/carousel4.jpg';
+import image5 from '../../assets/images/carousel5.jpg';
+
+function dashboardCarousel() {
+    const [index, setIndex] = useState(0);
+
+    const handleSelect = (selectedIndex, e) => {
+        setIndex(selectedIndex);
+    };
+
+    const images = [
+        image1,
+        image2,
+        image3,
+        image4,
+        image5
+    ];
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setIndex((prevIndex) => (prevIndex + 1) % images.length);
+        }, 3000);
+
+        return () => clearInterval(interval);
+    }, [images.length]);
+
+    return (
+        <div style={{ maxWidth: '300px', margin: 'auto' }}>
+            <Carousel activeIndex={index} onSelect={handleSelect} interval={null}>
+                {images.map((image, i) => (
+                    <Carousel.Item key={i}>
+                        <img
+                            className="d-block w-100"
+                            src={image}
+                            alt={`Slide ${i}`}
+                            style={{ height: 'auto', maxWidth: '100%' }}
+                        />
+                    </Carousel.Item>
+                ))}
+            </Carousel>
+            <Button variant="primary" onClick={() => setIndex((index + 1) % images.length)}>
+                Next Image
+            </Button>
+        </div>
+    );
+}
+
+export default dashboardCarousel
