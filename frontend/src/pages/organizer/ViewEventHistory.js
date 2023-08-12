f5ac9cb11e6882ef84771fcecb9d8d3ab96e8c5e
Nirodha <2020cs064@stu.ucsc.cmb.ac.lk>
2023-08-12 23:33:42 +0530

    commits

--- frontend/src/pages/organizer/ViewEventHistory.js
+++ frontend/src/pages/organizer/ViewEventHistory.js
@@ -1,25 +1,46 @@
 import React from 'react'
+import Topbar from '../../components/common/Topbar';
+import './ViewEventHistory.css';
 
 function ViewEventHistory() {
     return (
         <div className='view-event-history-container'>
-            <div className='hist-table d-flex flex-column justify-content-center text-center'>
-                <div className='title-row history-table-data-row'>
+            <Topbar></Topbar>
+            <div className='hist-table d-flex flex-column '>
+                <div className='title-row history-table-data-row d-flex mb-4'>
                     <div className='event-name'>Event Name</div>
                     <div className='event-type'>Event Type</div>
                     <div className='event-location'>Location</div>
                     <div className='event-date'>Date</div>
                 </div>
 
-                <div className='date-row history-table-data-row'>
-                    <div className='event-name'>Event Name</div>
-                    <div className='event-type'>Event Type</div>
-                    <div className='event-location'>Location</div>
-                    <div className='event-date'>Date</div>
+                <div className='data-row history-table-data-row d-flex'>
+                    <div className='event-name'>Nadagama</div>
+                    <div className='event-type'>Musical Show</div>
+                    <div className='event-location'>Mathara</div>
+                    <div className='event-date'>2023/03/09</div>
+                </div>  
+                <div className='data-row history-table-data-row d-flex'>
+                    <div className='event-name'>Aluth Kalawak</div>
+                    <div className='event-type'>Musical Show</div>
+                    <div className='event-location'>Mathara</div>
+                    <div className='event-date'>2023/03/09</div>
+                </div>  
+                <div className='data-row history-table-data-row d-flex'>
+                    <div className='event-name'>Nadagama</div>
+                    <div className='event-type'>Musical Show</div>
+                    <div className='event-location'>Mathara</div>
+                    <div className='event-date'>2023/03/09</div>
+                </div>  
+                <div className='data-row history-table-data-row d-flex'>
+                    <div className='event-name'>Nadagama</div>
+                    <div className='event-type'>Musical Show</div>
+                    <div className='event-location'>Mathara</div>
+                    <div className='event-date'>2023/03/09</div>
                 </div>    
             </div>
 
-            OrganizerEventHistory
+           
         </div>
     )
 }
