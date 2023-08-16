0d36f99763a634f8342374a26cba3ce4ea770050
Umasha Kaumadi <105776548+umasha2000@users.noreply.github.com>
2023-08-17 02:26:31 +0530

    Committing Changes

--- frontend/src/pages/organizer/OrganizerEventDashboard.js
+++ frontend/src/pages/organizer/OrganizerEventDashboard.js
@@ -15,6 +15,7 @@ import ViewArtist from './ViewArtist';
 import MakeArtistRequest from './MakeArtistRequest';
 import OrganizerDashboard from './OrganizerDashboard';
 
+
 import { BiSolidEdit } from "react-icons/bi";
 import { BiSolidBox } from "react-icons/bi";
 import { BiSolidCalendar } from "react-icons/bi";
@@ -28,6 +29,11 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import profilePhoto from '../../assets/images/profilePhoto.jpeg'
 import locationDemo from '../../assets/images/locationDemo.jpeg'
 import EventBanner4 from '../../assets/images/eventbanner4.jpg'
+import PaymentArtist4 from '../../assets/images/paymentArtist4.jpg'
+import PaymentArtist5 from '../../assets/images/paymentArtist5.jpeg'
+import PaymentArtist6 from '../../assets/images/paymentArtist6.jpg'
+import PaymentArtist7 from '../../assets/images/paymentArtist7.jpg'
+import PaymentArtist8 from '../../assets/images/paymentArtist8.png'
 
 
 function OrganizerEventDashboard() {
@@ -97,7 +103,7 @@ function OrganizerEventDashboard() {
 
                         <div className="locationDescriptionDiv mt-3 col-md-4">
                             <div className="locationTypeDescriptionDiv">
-                                <img className='img-fluid' src={locationDemo} alt='' ></img>
+                                <img className='' src={locationDemo} alt='' ></img>
                             </div>
                             <div className="eventTypeDescriptionDiv " style={{ fontFamily: 'MyCustomFont1' }}>
                                 <div className="p-3">
@@ -138,12 +144,12 @@ function OrganizerEventDashboard() {
                                     <div className="row tableContent">
                                         <div className="column">
                                             <div className="content">
-                                                <img alt='' src={profilePhoto} width='35px' height='35px' className='mx-4' />
-                                                Coldplay
+                                                <img alt='' src={PaymentArtist4} width='45px' height='45px' className='mx-4' />
+                                                Dim3
                                             </div>
                                         </div>
                                         <div className="column">
-                                            <div className="content confirmed mt-1" style={{ fontFamily: 'MyCustomFont2' }}>
+                                            <div className="content confirmed d-flex align-items-center justify-content-center mt-2" style={{ fontFamily: 'MyCustomFont2' }}>
                                                 Confirmed <BiSolidCircle className='mx-2' />
                                             </div>
                                         </div>
@@ -151,12 +157,13 @@ function OrganizerEventDashboard() {
                                     <div className="row tableContent">
                                         <div className="column">
                                             <div className="content">
-                                                Ed Sheeran
+                                                <img alt='' src={PaymentArtist5} width='45px' height='45px' className='mx-4' />
+                                                Ravi Royster
                                             </div>
                                         </div>
                                         <div className="column">
-                                            <div className="content">
-                                                Active
+                                            <div className="content d-flex align-items-center justify-content-center mt-2" style={{ color: 'orange', fontFamily: 'MyCustomFont2' }}>
+                                                Pending <BiSolidCircle className='mx-2' style={{ color: 'orange' }} />
                                             </div>
                                         </div>
 
@@ -165,12 +172,13 @@ function OrganizerEventDashboard() {
                                     <div className="row tableContent">
                                         <div className="column">
                                             <div className="content">
-                                                Ed Sheeran
+                                                <img alt='' src={PaymentArtist6} width='45px' height='45px' className='mx-4' />
+                                                MidLane
                                             </div>
                                         </div>
                                         <div className="column">
-                                            <div className="content">
-                                                Active
+                                            <div className="content d-flex align-items-center justify-content-center mt-2" style={{ color: 'red', fontFamily: 'MyCustomFont2' }} >
+                                                Rejected<BiSolidCircle className='mx-2' style={{ color: 'red' }} />
                                             </div>
                                         </div>
 
@@ -179,12 +187,13 @@ function OrganizerEventDashboard() {
                                     <div className="row tableContent">
                                         <div className="column">
                                             <div className="content">
-                                                Ed Sheeran
+                                                <img alt='' src={PaymentArtist5} width='45px' height='45px' className='mx-4' />
+                                                B N S
                                             </div>
                                         </div>
                                         <div className="column">
-                                            <div className="content">
-                                                Active
+                                            <div className="content d-flex align-items-center justify-content-center mt-2" style={{ color: 'orange', fontFamily: 'MyCustomFont2' }}>
+                                                Pending <BiSolidCircle className='mx-2' style={{ color: 'orange' }} />
                                             </div>
                                         </div>
 
@@ -193,15 +202,15 @@ function OrganizerEventDashboard() {
                                     <div className="row tableContent">
                                         <div className="column">
                                             <div className="content">
-                                                Ed Sheeran
+                                                <img alt='' src={PaymentArtist8} width='45px' height='45px' className='mx-4' />
+                                                Dinesh Gamage
                                             </div>
                                         </div>
                                         <div className="column">
-                                            <div className="content">
-                                                Active
+                                            <div className="content confirmed d-flex align-items-center justify-content-center mt-2" style={{ fontFamily: 'MyCustomFont2' }}>
+                                                Confirmed <BiSolidCircle className='mx-2' />
                                             </div>
                                         </div>
-
                                     </div>
 
 
@@ -226,16 +235,16 @@ function OrganizerEventDashboard() {
 
                             <div className="addTypeDescriptionDiv p-2" style={{ fontFamily: 'MyCustomFont1' }}>
                                 <div className="row col-md-12 ">
-                                    <Link to="/organizer/searchartist" className='link1'>
+                                    <Link to="/organizer/searchartist" style={{ color: 'white', textDecoration: 'none' }}>
                                         <div className="addRow mt-2 p-2 text-center">Add Artist
                                             <BiSolidPlusCircle className='plusIcon mx-2 fs-3' />
                                         </div>
                                     </Link>
 
-                                    <Link to="/organizer/searchartist">
-                                    <div className="addRow p-2 mt-2 text-center">Add Band
-                                        <BiSolidPlusCircle className='plusIcon mx-2 fs-3' />
-                                    </div>
+                                    <Link to="/organizer/searchartist" style={{ color: 'white', textDecoration: 'none' }}>
+                                        <div className="addRow p-2 mt-2 text-center">Add Band
+                                            <BiSolidPlusCircle className='plusIcon mx-2 fs-3' />
+                                        </div>
                                     </Link>
 
                                 </div>
