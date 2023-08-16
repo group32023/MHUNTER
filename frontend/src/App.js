637d6e55397d01306fde892f3bf0a69e3baf74e6
sandali kavishka <sandalikavish@gmail.com>
2023-08-16 15:26:10 +0530

    updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,6 +18,7 @@ import BandInvoice from './pages/BandInvoice';
 import ArtistEventOn from './pages/ArtistEventOn';
 import ArtistSpecificEvent from './pages/ArtistSpecificEvent';
 import ArtistGenerateReportsToday from './pages/ArtistGenerateReportsToday'
+import ArtistInvoice from './pages/ArtistInvoice';
 
 
 
@@ -39,13 +40,15 @@ function App() {
           <Route path='/artist/PendingRequests' element={<ArtistPendingRequests/>}></Route>
           <Route path='/artist/Reports' element={<ArtistGenerateReports/>}></Route>
           <Route path='/artist/Dashboard' element={<ArtistDashboard/>}></Route>
-          <Route path='/artist/report/today/:id' element={<ArtistGenerateReportsToday/>}></Route>
+          <Route path='/artist/Reports/:id' element={<ArtistGenerateReportsToday/>}></Route>
           <Route path='/artist/Event' element={<ArtistEvent/>}></Route>
           <Route path='/artist/EventSpecific/:id' element={<ArtistSpecificEvent/>}></Route>
           <Route path='/artist/PendingRequestView/:id' element={<ArtistPendingRequestView/>}></Route>
           <Route path='/artist/priorbooking/:id1/:id2' element={<ArtistPriorBooking/>}></Route>
-          <Route path='/artist/eventsOn/:mmid/:date/:id' element={<ArtistEventOn/>}></Route>
+          <Route path='/artist/eventsOn/:mmid/:date' element={<ArtistEventOn/>}></Route>
           <Route path='/band/invoice/:id' element={<BandInvoice/>}></Route> 
+          <Route path='/artist/invoice/:id' element={<ArtistInvoice/>}></Route> 
+
           
           <Route path='/artist/MyFeed' element={<MyFeed/>}></Route>
         </Routes>
