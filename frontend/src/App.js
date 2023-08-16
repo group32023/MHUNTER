7fdf2b6660ea231abb0eef08f1690d637eadb5fd
Umasha Kaumadi <105776548+umasha2000@users.noreply.github.com>
2023-08-16 10:07:13 +0530

    Committing Changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -60,63 +60,62 @@ function App() {
       <Router>
         <Routes>
           {/* Organizer Routes */}
-          <Route path='/' element={<Home />}>
+          <Route path='/organizer/' element={<OrganizerDashboard />}>
 
             <Route path='/organizer/dashboard' element={<OrganizerDashboardContent />}></Route>
             <Route path='/organizer/event' element={<ViewEvents />}></Route>
-            <Route path='/organizer/event/CreateEvent' element={<CreateEvent/>}></Route>
+            <Route path='/organizer/event/CreateEvent' element={<CreateEvent />}></Route>
             <Route path='/organizer/eventhistory' element={<OrganizerEventHistory />}></Route>
             <Route path='/organizer/complaint' element={<OrganizerComplaint />}></Route>
             <Route path='/organizer/profile' element={<OrganizerProfile />}></Route>
             <Route path='/organizer/searchartist' element={<SearchArtist />} />
           </Route>
 
-
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path='/artistbandsignup' element={<ArtistBandSignup />} />
           <Route path='/artistsignup' element={<ArtistSignup />} />
-          <Route path='/bandsignup' element={<BandSignup/>} />
-          <Route path='/organizersignup' element ={<OrganizerSignup/>} />
-          <Route path='/artistdashboard' element={<ArtistDashboard/>} />
-          <Route path='/banddashboard' element={<BandDashboard/>} />
-          <Route path='/organizerdashboard' element={<OrganizerDashboard/>} />
-          <Route path='/contact' element={<Contact/>}></Route>
-          <Route path='/artistDetails' element={<ArtistDetails/>}></Route>
-          <Route path='/post' element={<Post/>}></Route>
-          <Route path='/video' element={<Video/>}></Route>
+          <Route path='/bandsignup' element={<BandSignup />} />
+          <Route path='/organizersignup' element={<OrganizerSignup />} />
+          <Route path='/artistdashboard' element={<ArtistDashboard />} />
+          <Route path='/banddashboard' element={<BandDashboard />} />
+          <Route path='/organizerdashboard' element={<OrganizerDashboard />} />
+          <Route path='/contact' element={<Contact />}></Route>
+          <Route path='/artistDetails' element={<ArtistDetails />}></Route>
+          <Route path='/post' element={<Post />}></Route>
+          <Route path='/video' element={<Video />}></Route>
           <Route index element={<Home />} />
 
 
           {/*Admin*/}
-          <Route path='/admin/admindashboard' element={<AdminDashboard/>} />
-          <Route path='/admin/registration' element={<AdminRegistration/>} />
-          <Route path='/admin/registration/proofcheck' element={<ProofCheck/>} />
-          <Route path='/admin/Alluserdetails' element={<UserDetails/>} />
-          <Route path='/admin/userdetails' element={<AllUserDetails/>} />
-          <Route path='/admin/userdetails/viewdetails' element={<ViewUserDetails/>} />
-          <Route path='/admin/report' element={<AdminReport/>} />
-          <Route path='/admin/settings' element={<AdminSettings/>} />
-    
-                   {/* moderator */}
-          <Route path='/' element={<ModeratorDashboard/>}>
-          <Route path='/moderator/moderatorDashboard' element={<ModeratorDashboard/>} />
-          <Route path='/moderator/moderatorDashoboardContent' element={<ModeratorDashoboardContent />}></Route>
-          <Route path='/moderator/registration' element={<ModeratorRegistration  />}></Route>
-          <Route path='/moderator/registration/ProofCheck' element={<ProofCheck/>} />
-          <Route path='/moderator/event' element={<ModeratorEvent />}></Route>
-          <Route path='/moderator/complaints' element={<ModeratorComplaints />}></Route>
-          <Route path='/moderator/complaints/viewComplaints' element={<ViewComplaints/>} />
-          <Route path='/moderator/userDetails' element={<ModeratorUserDetails />}></Route>
-          <Route path='/moderator/event/eventDetails' element={<EventDetails/>}/>
-          <Route path='/moderator/registration/ProofCheck' element={<proofCheck/>}/>
-          <Route path='/moderator/registration/proofcheck/suspenduser' element={<Suspenduser/>}/>
+          <Route path='/admin/admindashboard' element={<AdminDashboard />} />
+          <Route path='/admin/registration' element={<AdminRegistration />} />
+          <Route path='/admin/registration/proofcheck' element={<ProofCheck />} />
+          <Route path='/admin/Alluserdetails' element={<UserDetails />} />
+          <Route path='/admin/userdetails' element={<AllUserDetails />} />
+          <Route path='/admin/userdetails/viewdetails' element={<ViewUserDetails />} />
+          <Route path='/admin/report' element={<AdminReport />} />
+          <Route path='/admin/settings' element={<AdminSettings />} />
+
+          {/* moderator */}
+          <Route path='/' element={<ModeratorDashboard />}>
+            <Route path='/moderator/moderatorDashboard' element={<ModeratorDashboard />} />
+            <Route path='/moderator/moderatorDashoboardContent' element={<ModeratorDashoboardContent />}></Route>
+            <Route path='/moderator/registration' element={<ModeratorRegistration />}></Route>
+            <Route path='/moderator/registration/ProofCheck' element={<ProofCheck />} />
+            <Route path='/moderator/event' element={<ModeratorEvent />}></Route>
+            <Route path='/moderator/complaints' element={<ModeratorComplaints />}></Route>
+            <Route path='/moderator/complaints/viewComplaints' element={<ViewComplaints />} />
+            <Route path='/moderator/userDetails' element={<ModeratorUserDetails />}></Route>
+            <Route path='/moderator/event/eventDetails' element={<EventDetails />} />
+            <Route path='/moderator/registration/ProofCheck' element={<proofCheck />} />
+            <Route path='/moderator/registration/proofcheck/suspenduser' element={<Suspenduser />} />
           </Route>
 
-    
-      </Routes>
-    </Router>
-      
+
+        </Routes>
+      </Router>
+
       {/*<div>
       <MainSlider>
       </MainSlider>
