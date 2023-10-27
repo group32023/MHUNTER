10bc358dce9488fcd12c247e736409e65c81dbe7
Umasha Kaumadi <105776548+umasha2000@users.noreply.github.com>
2023-10-27 13:29:20 +0530

    Committing Changes

--- backend/src/main/java/com/MHunter/mhunter/repository/RequestMusicMemberRepository.java
+++ backend/src/main/java/com/MHunter/mhunter/repository/RequestMusicMemberRepository.java
@@ -2,6 +2,7 @@ package com.MHunter.mhunter.repository;
 
 import com.MHunter.mhunter.model.RequestMusicMember;
 import com.MHunter.mhunter.model.RequestMusicMemberId;
+import com.MHunter.mhunter.struct.EventRequestStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -29,5 +30,8 @@ public interface RequestMusicMemberRepository extends JpaRepository<RequestMusic
     @Query(value = "SELECT * FROM request_music_member WHERE mmid = :mmid AND org_id= :orgId AND confirmation_status = 1", nativeQuery = true)
     List<RequestMusicMember> findByMMIDListForAOrganizer(@Param("mmid") int mmid,@Param("orgId") int orgId);
 
+    List<EventRequestStatus> findByOrgIdAndEventId(Integer orgId, Integer eventId);
+    @Query("SELECT rmm.mmid, rmm.orgId, rmm.confirmationStatus, rmm.eventId, mm.userId, CONCAT(u.firstName, ' ', u.lastName) AS fullName FROM RequestMusicMember AS rmm JOIN MusicMember AS mm ON rmm.mmid = mm.mmid JOIN User AS u ON mm.userId = u.userId WHERE rmm.orgId = :orgId AND rmm.eventId = :eventId")
+    List<EventRequestStatus> customQuery(@Param("orgId") Long orgId, @Param("eventId") Long eventId);
 
 }
--- backend/src/main/java/com/MHunter/mhunter/service/EventRequestStatusService.java
+++ backend/src/main/java/com/MHunter/mhunter/service/EventRequestStatusService.java
@@ -1,7 +1,4 @@
-//import com.MHunter.mhunter.struct.EventRequestStatus;
-//
-//import java.util.List;
-//
-//public interface EventRequestStatusService {
-//    List<EventRequestStatus> getEventRequestStatus();
-//}
+package com.MHunter.mhunter.service;
+
+public interface EventRequestStatusService {
+}
