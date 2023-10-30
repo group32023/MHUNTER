41bbd910ca3d136b0242fb8524dfb25b40750575
theekshani99 <theekshani3@gmail.com>
2023-10-30 15:27:15 +0530

    update complaints

--- frontend/src/pages/moderator/complaints.js
+++ frontend/src/pages/moderator/complaints.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { Routes, Route } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom'; // Import routing components
 
 import SideMenuBarModerator from '../../components/common/SideMenuBar/SideMenuBarModerator'
 import Topbar from '../../components/common/Topbar'
@@ -15,7 +15,7 @@ import band from '../../assets/images/band.jpg'
 import ModeratorRegistration from './registration';
 import ModeratorComplaints from './complaints';
 import ModeratorUserDetails from './userDetails';
-import ModeratorDashoboardContent from './moderatorDashoboardContent';
+// import ModeratorDashoboardContent from './moderatorDashoboardContent';
 import EventDetails from './eventDetails';
 import ModeratorEvent from './event';
 import ViewComplaints from './viewComplaints';
@@ -23,181 +23,103 @@ import ProofCheck from './ProofCheck';
 import Suspenduser from './suspenduser';  
 
 function Complaints() {
-    
-    const [currentPage, setCurrentPage] = useState(0);
-    const itemsPerPage = 5;
-  
-    const data = [
-      {
-        complainId: 'C1001',
-        name: 'Karen Smith',
-        title: 'Refund Payment',
-        date: '2021/07/09',
-        status: 'Completed',
-      },
-      {
-        complainId: 'C1002',
-        name: 'John Doe',
-        title: 'Refund Payment',
-        date: '2021/08/15',
-        status: 'Pending',
-      },
-      {
-        complainId: 'C1003',
-        name: 'Alice Johnson',
-        title: 'Refund Payment',
-        date: '2021/09/20',
-        status: 'Checking',
-      },
-      {
-        complainId: 'C1004',
-        name: 'Refund Payment',
-        title: 'Late Delivery',
-        date: '2021/10/05',
-        status: 'Completed',
-      },
-      {
-        complainId: 'C1005',
-        name: 'Refund Payment',
-        title: 'Wrong Size',
-        date: '2021/11/17',
-        status: 'Pending',
-      },
-      {
-        complainId: 'C1006',
-        name: 'Refund Payment',
-        title: 'Billing Issue',
-        date: '2021/12/02',
-        status: 'Checking',
-      },
-        {
-            complainId: 'C1001',
-            name: 'Karen Smith',
-            title: 'Refund Payment',
-            date: '2021/07/09',
-            status: 'Completed',
-          },
-          {
-            complainId: 'C1002',
-            name: 'John Doe',
-            title: 'Refund Payment',
-            date: '2021/08/15',
-            status: 'Pending',
-          },
-          {
-            complainId: 'C1003',
-            name: 'Alice Johnson',
-            title: 'Refund Payment',
-            date: '2021/09/20',
-            status: 'Checking',
-          },
-          {
-            complainId: 'C1004',
-            name: 'Refund Payment',
-            title: 'Late Delivery',
-            date: '2021/10/05',
-            status: 'Completed',
-          },
-          {
-            complainId: 'C1005',
-            name: 'Refund Payment',
-            title: 'Wrong Size',
-            date: '2021/11/17',
-            status: 'Pending',
-          },
-          {
-            complainId: 'C1006',
-            name: 'Refund Payment',
-            title: 'Billing Issue',
-            date: '2021/12/02',
-            status: 'Checking',
-          },
-    ];
-  
-    const startIndex = currentPage * itemsPerPage;
-    const endIndex = startIndex + itemsPerPage;
-    const displayedData = data.slice(startIndex, endIndex);
-  
-    const handlePageChange = ({ selected }) => {
-      setCurrentPage(selected);
-    };
-  
+  const [currentPage, setCurrentPage] = useState(0);
+  const itemsPerPage = 5;
+  const [complaints, setComplaints] = useState([]); // State to store fetched data
+
+  const navigate = useNavigate(); // React Router navigation
+
+  // Fetch complaint data from the backend
+  useEffect(() => {
+    // You can use the fetch API or Axios to make the HTTP GET request
+    fetch('http://localhost:8080/complaint/getAll') // Replace with your actual backend endpoint
+      .then((response) => response.json())
+      .then((data) => setComplaints(data))
+      .catch((error) => console.error('Error fetching data:', error));
+  }, []); // Empty dependency array ensures the effect runs once on component mount
+
+  const startIndex = currentPage * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const displayedData = complaints.slice(startIndex, endIndex);
+
+  const handlePageChange = ({ selected }) => {
+    setCurrentPage(selected);
+  };
+
   return (
     <>
-    <SideMenuBarModerator>
-    <Topbar/>
-    <div className='moderator-body-container'>
-
-            {/*header icon*/}
-            <div className='header-admin'>
-
-                <div className='header-title'>
-                    <h1>Complaints</h1>
-                </div>
-
-                
-
+      <SideMenuBarModerator>
+        <Topbar />
+        <div className="moderator-body-container">
+          {/*header icon*/}
+          <div className="header-admin">
+            <div className="header-title">
+              <h1>Complaints</h1>
             </div>
-            
-            {/*table*/}
-            <table className='data-table-complaints'>
-    <thead>
-        <tr>
-            <th>Complain ID</th>
-            <th>Name</th>
-            <th>Title</th>
-            <th>Date</th>
-            <th>Status</th>
-            <th></th>
-        </tr>
-    </thead>
-    <tbody>
-          {displayedData.map((item, index) => (
-            <tr key={index} className='data-row-complaints'>
-              <td className='data-name-complaints'>{item.complainId}</td>
-              <td className='data-name-complaints'>{item.name}</td>
-              <td className='data-name-complaints'>{item.title}</td>
-              <td className='data-name-complaints'>{item.date}</td>
-              <td className={`data-name-complaints-${item.status}`}>{item.status}</td>
-              <td>
-                <Link to={`/moderator/complaints/viewComplaints`} className='data-button2'>
-                  <button type='button' className='btn btn-primary2'>View</button>
-                </Link>
-              </td>
-            </tr>
-          ))}
-        </tbody>
+          </div>
 
-</table>
-<div className="pagination-wrapper">
-  <ReactPaginate
-  previousLabel={<BsArrowLeftCircle/>} 
-  nextLabel={<BsArrowRightCircle/>}
-    pageCount={Math.ceil(data.length / itemsPerPage)}
-    onPageChange={handlePageChange}
-    containerClassName={'pagination'}
-    activeClassName={'active2'}
-  />
-</div>
-
-        </div> 
-        <Routes>
-          {/* Nested routes for the Organizer Dashboard */}
-          <Route path='/moderator/moderatorDashoboardContent' element={<ModeratorDashoboardContent />}></Route>
-          <Route path='/moderator/registration' element={<ModeratorRegistration />}></Route>
-          <Route path='/moderator/complaints' element={<ModeratorComplaints />}></Route>
-          <Route path='/moderator/complaints/viewComplaints' element={<ViewComplaints />}></Route>
-
-          <Route path='/moderator/userDetails' element={<ModeratorUserDetails />}></Route>
-          <Route path='/moderator/event' element={<ModeratorEvent />}></Route>
-          <Route path='/moderator/event/eventDetails' element={<EventDetails />}></Route>
-          <Route path='/moderator/registration/ProofCheck' element={<ProofCheck />}></Route>
-          <Route path='/moderator/registration/Proofcheck/suspenduser' element={<Suspenduser />}></Route>
-
-        </Routes>
+          {/*table*/}
+          <table className="data-table-complaints">
+            <thead>
+              <tr>
+                <th>Complain ID</th>
+                <th>Name</th>
+                <th>Title</th>
+                <th>Date</th>
+                <th>Status</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {displayedData.map((item, index) => (
+                <tr key={index} className="data-row-complaints">
+                  <td className="data-name-complaints">{item.complaintID}</td>
+                  <td className="data-name-complaints">{item.name}</td>
+                  <td className="data-name-complaints">{item.title}</td>
+                  <td className="data-name-complaints">{item.date}</td>
+                  <td className={`data-name-complaints-${item.status}`}>{item.status}</td>
+                  <td>
+                    {/* Use the navigate function to handle the link */}
+                    <button
+                      onClick={() => navigate(`/moderator/complaints/viewComplaints`)}
+                      className="data-button2"
+                    >
+                      View
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          
+          {/* Pagination component */}
+          <div className="pagination-wrapper">
+            <ReactPaginate
+              previousLabel={<BsArrowLeftCircle />}
+              nextLabel={<BsArrowRightCircle />}
+              pageCount={Math.ceil(complaints.length / itemsPerPage)}
+              onPageChange={handlePageChange}
+              containerClassName={'pagination'}
+              activeClassName={'active2'}
+            />
+          </div>
+        </div>
       </SideMenuBarModerator>
-          </>
-  )
+
+      {/* Nested routes for the Moderator Dashboard */}
+      <Routes>
+        {/* <Route path="/moderator/moderatorDashboardContent" element={<ModeratorDashboardContent />} /> */}
+        <Route path="/moderator/registration" element={<ModeratorRegistration />} />
+        <Route path="/moderator/complaints" element={<ModeratorComplaints />} />
+        <Route path="/moderator/complaints/viewComplaints" element={<ViewComplaints />} />
+        <Route path="/moderator/userDetails" element={<ModeratorUserDetails />} />
+        <Route path="/moderator/event" element={<ModeratorEvent />} />
+        <Route path="/moderator/event/eventDetails" element={<EventDetails />} />
+        <Route path="/moderator/registration/ProofCheck" element={<ProofCheck />} />
+        <Route path="/moderator/registration/ProofCheck/suspenduser" element={<Suspenduser />} />
+      </Routes>
+    </>
+  );
 }
 
-export default Complaints
+export default Complaints;
