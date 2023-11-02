e67075432836dad22f2c08ab366e1613b0146414
Umasha Kaumadi <105776548+umasha2000@users.noreply.github.com>
2023-11-02 09:38:04 +0530

    Committing Changes

--- frontend/src/components/organizer/AgreementDetailProfile.js
+++ frontend/src/components/organizer/AgreementDetailProfile.js
@@ -1,28 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
 import '../../assets/css/AgreementDetailProfile.css'
 
-const AgreementDetailProfile = () => {
+const AgreementDetailProfile = (props) => {
+  const detail0 = props.detail0;
+  const mmid = detail0;
+  useEffect(() => {
+    const apiUrl = `http://localhost:8080/requestMusicMember/musicMemberDetails/${mmid}`;
+    axios.get(apiUrl)
+      .then((response) => {
+        setMusicMember(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+  const [musicMember, setMusicMember] = useState({});
   return (
-    <div className="container mt-4 mb-4 p-3 d-flex justify-content-center">
+    <div className="container mb-4 p-3 d-flex justify-content-center">
       <div className="agreementDetail card p-4">
         <div className="image d-flex flex-column justify-content-center align-items-center">
-          <button className="btn btn-secondary">
+          <button className="ppbtn btn-secondary">
             <img src="https://i.imgur.com/wvxPV9S.png" height="100" width="100" alt="Profile" />
           </button>
-          <span className="name mt-3">Eleanor Pena</span>
-          <span className="idd">@eleanorpena</span>
+          <span className="name mt-3">{musicMember.userName}</span>
+          <span className="idd">@{musicMember.userName}</span>
           <div className="d-flex flex-row justify-content-center align-items-center gap-2">
-            <span className="idd1">Oxc4c16a645_b21a</span>
+            <span className="idd1">{musicMember.email}</span>
             <span><i className="fa fa-copy"></i></span>
           </div>
           <div className="d-flex flex-row justify-content-center align-items-center mt-3">
             <span className="number">1069 <span className="follow">Followers</span></span>
           </div>
           <div className="d-flex mt-2">
-            <button className="btn1 btn-dark">Edit Profile</button>
+            <button className="btn1 btn-dark">View Profile</button>
           </div>
           <div className="text mt-3">
-            <span>Eleanor Pena is a creator of minimalistic x bold graphics and digital artwork.
+            <span>{musicMember.userName} is a creative and talented individual or group that specializes in producing and performing musical compositions.
               <br /><br /> Artist/ Creative Director by Day #NFT minting@ with FND night. </span>
           </div>
           <div className="gap-3 mt-3 icons d-flex flex-row justify-content-center align-items-center">
@@ -32,7 +48,7 @@ const AgreementDetailProfile = () => {
             <span><i className="fa fa-linkedin"></i></span>
           </div>
           <div className="px-2 rounded mt-4 date">
-            <span className="join">Joined May, 2021</span>
+            <span className="join">{musicMember.email}</span>
           </div>
         </div>
       </div>
